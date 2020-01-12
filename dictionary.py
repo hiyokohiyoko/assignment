0266c1b5f4e304636c2d5f234c4645777d82fb51
hiyokohiyoko <m1716072hk@ug.jwu.ac.jp>
2020-01-12 16:24:20 +0900

    added words

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,60 @@
-# subject -100~100
+# subject -100~100 エンタメ系~学問・政治系
 dic1 = {
-
+    'idol' : -90,
+    'desney' : -90,
+    'fashion' : -50,
+    'flower' : -30,
+    'music' : 0,
+    'TV' : 0,
+    'suits' : 30,
+    'weather' : 30,
+    'news' : 30,
+    'python' : 50,
+    'python3' : 50,
+    'java' : 50,
+    'C' : 50,
+    'computer' : 60,
+    'economy' : 80,
+    'politics' : 80,
+    'math' : 80,
+    'mathematics' : 80,
+    'science' : 80,
+    'government' : 80,
+    'war' : 90,
+    'peace' : 90,
 }
 
-# interest
+# interest -100~100 全く興味がない~とても興味がある
 dic2 = {
-
+    'am_tired_of' : -80,
+    'am_not_big_on' : -50,
+    'am_not_into' : -40,
+    'curious_about' : 60,
+    'get_curious_about' : 60,
+    'have_interest_in' : 80,
+    'am_interested_in' : 80,
+    'be_interested_in' : 80,
 }
 
-# likability
+# likability -100~100 とても嫌い~とても好き
 dic3 = {
-
+    'loathe' : -100,
+    'abominate' : -100,
+    'detest' : -90,
+    'hate' : -80,
+    'dispise' : -80,
+    'dislike' : -70,
+    'abhor' : -60,
+    'am_so-so_about' : 0,
+    'like' : 50,
+    'exciting' : 60,
+    'am_involved_in' : 70,
+    'am_fond_of' : 80,
+    'am_attached_to' : 80,
+    'am_keen_on' : 90,
+    'love' : 90,
+    'adore' : 95,
+    'am_crazy_about' : 90,
+    'am_passionate_about' : 100,
+    'am_partial_to' : 100,
 }
